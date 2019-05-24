2db605aa5125c7b4795896115ea89fee65018450
min1744 <dkqtk@naver.com>
2019-05-24 19:42:35 +0900

    aa

--- src/com/iu/page/Search.java
+++ src/com/iu/page/Search.java
@@ -1,5 +1,36 @@
 package com.iu.page;
 
 public class Search {
+	
+	private String search;
+	private String kind;
+	
+	public String getSearch() {
+		return search;
+	}
+	public void setSearch(String search) {
+		if(search==null) {
+			search ="";
+		}else {
+			this.search = search;			
+		}
+	}
+	public String getKind() {
+		return kind;
+	}
+	public void setKind(String kind) {
+		this.kind = kind;
+		if(kind==null) {
+			kind="title";
+		}else if(kind.equals("c")) {
+			kind="contents";
+		}else if(kind.equals("w")) {
+			kind="writer";
+		}else {
+			kind="title";
+		}
+	}
+	
+	
 
 }
--- src/com/iu/page/SearchMakePage.java
+++ src/com/iu/page/SearchMakePage.java
@@ -1,5 +1,13 @@
 package com.iu.page;
 
 public class SearchMakePage {
+	private int perPage;
+	private int curPage;
+	private Search search;
+	
+	
+	public SearchMakePage(int curPage, String kind, String search) {
+		
+	}
 
 }
